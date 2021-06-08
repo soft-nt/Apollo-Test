10048f0680cbae4eef5ca8caa23bb0e4ad6a7128
NT <n.torrent@gmail.com>
2021-06-08 11:37:10 +0200

    Update

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
 
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+
+const client = new ApolloClient({
+  // options go here
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <GlobalStyles />
     <Pages />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,30 @@
-//TODO
+const {ApolloServer, MockList} = require('apollo-server');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+const TrackAPI = require('./datasources/track-api');
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => {
+      return {
+        trackAPI: new TrackAPI() 
+      };
+    },
+    context: ({req}) => {
+      const token = req.headers.authorization || '';
+      console.log(req.headers.authorization);
+        
+      if (token != 'Bearer 123')
+      throw new AuthenticationError('Please provide a correct bearer token');
+    }
+});
+
+
+server.listen().then(() => {
+    console.log(`
+      🚀  Server is running!
+      🔉  Listening on port 4000
+      📭  Query at https://studio.apollographql.com/dev
+    `);
+  });
